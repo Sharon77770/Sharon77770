01302b3aa96fff96066a9bcb6693c40ae4dd2718
주민재 <john_joo7@naver.com>
2024-11-25 15:25:35 +0900

    finished who am i

---
+++ js/contentAnime.js
@@ -0,0 +1,49 @@
+const whoAmIContent = document.querySelector('.who-am-i-container');
+const techContent = document.querySelector('.tech-stack-container');
+const techBars = document.querySelectorAll('.tech-bar');
+
+function whoAmIAnime() {
+    const rect = whoAmIContent.getBoundingClientRect();
+    const offset = 300;
+    const isVisible = rect.top < window.innerHeight - offset && rect.bottom > 0;
+
+    if(isVisible) {
+        whoAmIContent.classList.add('visible');
+    }
+}
+
+function techContentAnime() {
+    const rect = techContent.getBoundingClientRect();
+    const offset = 0;
+    const isVisible = rect.top < window.innerHeight - offset && rect.bottom > 0;
+
+    if(isVisible) 
+        techContent.classList.add('visible');
+}
+
+function techBarsAnime() {
+    setTimeout(() => {
+        techBars.forEach( (bar, idx) => {
+            setTimeout(() => {
+                const rect = bar.getBoundingClientRect();
+                const isVisible = rect.top < window.innerHeight && rect.bottom > 0;
+
+                if(isVisible){ 
+                    const targetWidth = bar.getAttribute('data-width');
+                    bar.style.width = targetWidth;
+                    bar.classList.add('visible')
+                }
+            }, idx * 300);
+        });
+    }, 0);
+}
+
+
+const handleScroll = () => {
+    whoAmIAnime();
+    techContentAnime();
+    techBarsAnime();
+}
+
+window.addEventListener('scroll', handleScroll);
+handleScroll();
